c672647eb650216afa15d9d4e1ada7122aa35302
Mauri <mauricio.lobos@engagemeapps.com>
2024-10-07 14:38:27 -0300

    commit inicial

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.proyectoFit.backend.apirest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
